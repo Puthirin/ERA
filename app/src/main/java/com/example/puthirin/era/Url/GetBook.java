b9a5c54ceb531c8783a2b2f112b15ada93ff0365
DESKTOP-8FPQISN\dell <gov hengly>
2017-10-10 15:56:32 +0700

    Push

---
+++ app/src/main/java/com/example/puthirin/era/Url/GetBook.java
@@ -0,0 +1,8 @@
+package com.example.puthirin.era.Url;
+
+/**
+ * Created by dell on 10/10/2017.
+ */
+
+public class getBook {
+}
---
+++ app/src/main/java/com/example/puthirin/era/Url/StringUrl.java
@@ -0,0 +1,10 @@
+package com.example.puthirin.era;
+
+/**
+ * Created by dell on 10/10/2017.
+ */
+
+public class StringUrl {
+    public String URl ="192.168.43.182";
+    public String Detail_BookID = URl+"";
+}
