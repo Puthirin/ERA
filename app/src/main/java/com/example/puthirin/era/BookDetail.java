c6c0f72cd8b8f540c4378f70894748cd4d208e31
DESKTOP-8FPQISN\dell <gov hengly>
2017-10-10 15:50:19 +0700

    Commit

--- app/src/main/java/com/example/puthirin/era/BookDetail.java
+++ app/src/main/java/com/example/puthirin/era/BookDetail.java
@@ -2,12 +2,19 @@ package com.example.puthirin.era;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.puthirin.era.Url.GetBook;
+import com.example.puthirin.era.Url.StringUrl;
 
 public class BookDetail extends AppCompatActivity {
-
+    StringUrl stringUrl = new StringUrl();
+    GetBook getBook = new GetBook();
+    TextView Title, Author, Description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+//        Title = (TextView)findViewById(R.id.t)
     }
 }
