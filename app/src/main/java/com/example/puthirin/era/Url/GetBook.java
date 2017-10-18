f953261d738adce3783d705401aa592e25b87c62
iMac-13 <imac-13@iMac-13s-iMac.mshome.net>
2017-10-18 16:45:14 +0700

    update

--- app/src/main/java/com/example/puthirin/era/Url/GetBook.java
+++ app/src/main/java/com/example/puthirin/era/Url/GetBook.java
@@ -4,5 +4,5 @@ package com.example.puthirin.era.Url;
  * Created by dell on 10/10/2017.
  */
 
-public class getBook {
+public class GetBook {
 }
--- app/src/main/java/com/example/puthirin/era/Url/StringUrl.java
+++ app/src/main/java/com/example/puthirin/era/Url/StringUrl.java
@@ -1,10 +1,11 @@
-package com.example.puthirin.era;
+package com.example.puthirin.era.Url;
 
 /**
  * Created by dell on 10/10/2017.
  */
 
 public class StringUrl {
+
     public String URl ="192.168.43.182";
     public String Detail_BookID = URl+"";
 }
